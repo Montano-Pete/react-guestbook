7b8e6337d532a7d6abb57ac49281c06db96d926c
Peter <pete225848@gmail.com>
2022-01-05 17:14:17 -0800

    created ProvideAuth context

---
+++ src/context/ProvideAuth.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from 'react';
+
+const authContext = createContext();
+
+function ProvideAuth({ children }) {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    const loginSuccessful =
+      email === process.env.AUTH_EMAIL &&
+      password === process.env.AUTH_PASSWORD;
+    if (loginSuccessful) setUser({ email });
+    return loginSuccessful;
+  };
+
+  const logout = (callback) => {
+    setUser(null);
+    callback();
+  };
+
+  return (
+    <authContext.Provider value={{ user, login, logout }}>
+      {children}
+    </authContext.Provider>
+  );
+}
+
+export { authContext, ProvideAuth };
